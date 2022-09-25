720fa7fdf577e2ee77487b98f5fd6686d4d48bf5
Nina2498 <shene1998m@gmail.com>
2022-09-25 15:21:33 +0300

    Add more features

--- lab1/client.java
+++ lab1/client.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.util.Scanner;
 //import java.util.concurrent.locks.ReadWriteLock;
 public class client {
+    //TODO: ADD MORE FEATURES
 
     public static void main(String[] args) {
         final BufferedReader in;
